ef93ed8ff50504f97daded90fecb860f0a0fedc1
Aditya-singh101 <vishusingh212301@gmail.com>
2025-09-09 17:20:33 +0530

    Initial commit

---
+++ chatbot/app.py
@@ -0,0 +1,7 @@
+from langchain_openai import chatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
